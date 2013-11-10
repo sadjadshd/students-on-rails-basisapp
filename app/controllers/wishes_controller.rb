e58f39be959adfaaea788b582b19cad4ef0742dc
Jonas2000 <jonashonisch@aol.com>
2013-11-10 13:01:12 +0100

    added funstionality to add wishe to to a wishlist

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -42,10 +42,7 @@ class WishesController < ApplicationController
   # DELETE /wishes/1.json
   def destroy
     @wish.destroy
-    respond_to do |format|
-      format.html { redirect_to wishes_url }
-      format.json { head :no_content }
-    end
+    redirect_to wishlist_path(@wish.wishlist_id)
   end
 
   private
