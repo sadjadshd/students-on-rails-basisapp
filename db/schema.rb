c8ec86cedb5825202cb6919de4ebe6aad37a4d59
Fabian Sawatzki <fabiansawatzki@googlemail.com>
2013-11-10 11:19:00 +0100

    Prohibited users to sign up without a first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109142211) do
+ActiveRecord::Schema.define(version: 20131110095209) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20131109142211) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
