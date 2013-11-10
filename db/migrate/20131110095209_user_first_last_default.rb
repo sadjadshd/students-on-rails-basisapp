92b37bd3adc719b261b2018d570285fe886cd940
Fabian Sawatzki <fabiansawatzki@googlemail.com>
2013-11-10 11:56:35 +0100

    another db fix....

--- db/migrate/20131110095209_user_first_last_default.rb
+++ db/migrate/20131110095209_user_first_last_default.rb
@@ -1,9 +1,13 @@
 class UserFirstLastDefault < ActiveRecord::Migration
   def up
-    User.where("first_name = ''").update_all(:first_name => "Santa")
-    User.where("last_name = ''").update_all(:last_name => "Clause")
+    User.where(first_name: nil).update_all(:first_name => "Santa")
+    User.where(last_name: nil).update_all(:last_name => "Clause")
+    change_column_null :users, :first_name, false
+    change_column_null :users, :last_name, false
   end
 
   def down
+    change_column_null :users, :first_name, true
+    change_column_null :users, :last_name, true
   end
 end
